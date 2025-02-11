b8a4cf13e5aacc1c378f5b84a37f58934e31ff7d
amirrajj-dev <amiramraja@gmail.com>
2025-02-11 21:43:00 +0330

    added daisy ui to the project🐘🩶

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-
 const App = () => {
   return (
-    <div className='bg-emerald-500 , p-4 m-4 rounded-md text-white'>App</div>
-  )
-}
+    <>
+      <div className="bg-emerald-500 , p-4 m-4 rounded-md text-white">App</div>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  );
+};
 
-export default App
+export default App;
