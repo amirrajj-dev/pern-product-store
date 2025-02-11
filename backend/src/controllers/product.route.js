52b061d64444e285d635e28334ab8cbb5636490d
amirrajj-dev <amiramraja@gmail.com>
2025-02-11 19:30:51 +0330

    get products functionallity completed🐘🩶

--- backend/src/controllers/product.route.js
+++ backend/src/controllers/product.route.js
@@ -1,5 +1,18 @@
+import {sql} from '../utils/db.js'
 
-export const getAllProducts = async (req, res) =>{}
+export const getAllProducts = async (req, res) =>{
+    try {
+        const result = await sql`SELECT * FROM products ORDER BY created_at DESC`
+        res.status(200).json({
+            data: result.rows,
+            success : true,
+            message: 'Products fetched successfully'
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: 'Server error' , success: false})
+    }
+}
 
 export const createProduct = async (req, res) =>{}
 
