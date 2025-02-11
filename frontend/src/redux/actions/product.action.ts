9355f33ecade48a6d8b14e8e3d6f0c5f0105de7b
amirrajj-dev <amiramraja@gmail.com>
2025-02-12 00:42:14 +0330

    update product actions🐘🩶

--- frontend/src/redux/actions/product.action.ts
+++ frontend/src/redux/actions/product.action.ts
@@ -1,8 +1,28 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { axiosInstance } from '../../configs/axios';
 import { Product } from '../types';
 
-export const getAllProducts = createAction<Product[]>('GET_ALL_PRODUCTS');
-export const createProduct = createAction<Product>('CREATE_PRODUCT');
-export const getOneProduct = createAction<Product>('GET_ONE_PRODUCT');
-export const updateProduct = createAction<Product>('UPDATE_PRODUCT');
-export const deleteProduct = createAction<number>('DELETE_PRODUCT');
+export const fetchAllProducts = createAsyncThunk<Product[]>('products/fetchAll', async () => {
+  const response = await axiosInstance.get('/products');
+  return response.data.data;
+});
+
+export const fetchProduct = createAsyncThunk<Product, number>('products/fetchOne', async (id) => {
+  const response = await axiosInstance.get(`/products/${id}`);
+  return response.data.data;
+});
+
+export const addProduct = createAsyncThunk<Product, Partial<Product>>('products/add', async (product) => {
+  const response = await axiosInstance.post('/products', product);
+  return response.data.data;
+});
+
+export const updateProduct = createAsyncThunk<Product, { id: number, product: Partial<Product> }>('products/update', async ({ id, product }) => {
+  const response = await axiosInstance.put(`/products/${id}`, product);
+  return response.data.data;
+});
+
+export const removeProduct = createAsyncThunk<number, number>('products/remove', async (id) => {
+  await axiosInstance.delete(`/products/${id}`);
+  return id;
+});
