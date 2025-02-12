db844c80223651b64fbd1a92b4c41889265ad1b5
amirrajj-dev <amiramraja@gmail.com>
2025-02-12 20:23:19 +0330

    necessary changes for deploying on render🐘🩶

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,7 @@ app.use(morgan("dev")) // log the request to the console
 app.use(express.json())
 app.use(cors({
     credentials: true, // enable cookies
-    origin : 'http://localhost:5173'
+    origin : process.env.FRONTEND_URL || 'http://localhost:5173'
 }))
 
 app.use(async (req, res, next) => {
