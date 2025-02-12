c8d642e5e4157f67bd3ee63fc3c2b3e60c325462
amirrajj-dev <amiramraja@gmail.com>
2025-02-12 16:44:52 +0330

    added arcjet status 429 to the fetch products functionallity🐘🩶

--- frontend/src/redux/actions/product.action.ts
+++ frontend/src/redux/actions/product.action.ts
@@ -2,10 +2,21 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { axiosInstance } from '../../configs/axios';
 import { Product } from '../types';
 
-export const fetchAllProducts = createAsyncThunk<Product[]>('products/fetchAll', async () => {
-  const response = await axiosInstance.get('/products');
-  return response.data.data;
-});
+export const fetchAllProducts = createAsyncThunk<Product[], void>(
+  'products/fetchAll',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.get('/products');
+      console.log(response);
+      return response.data.data;
+    } catch (error: any) {
+      if (error.response && error.response.status === 429) {
+        alert("You're making too many requests. Please slow down.");
+      }
+      return rejectWithValue(error.response?.data?.message || 'Error fetching products');
+    }
+  }
+);
 
 export const fetchProduct = createAsyncThunk<Product, number>('products/fetchOne', async (id) => {
   const response = await axiosInstance.get(`/products/${id}`);
